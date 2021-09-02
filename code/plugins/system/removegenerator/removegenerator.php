d32a9e16f8fc2f3e9757ce17e9d7263755b22294
SharkyKZ <sharkykz@gmail.com>
2021-09-02 09:49:10 +0300

    Initial commit

---
+++ code/plugins/system/removegenerator/removegenerator.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * @copyright   (C) 2021 SharkyKZ
+ * @license     GPL-2.0-or-later
+ */
+
+defined('_JEXEC') or exit;
+
+use Joomla\CMS\Document\FeedDocument;
+use Joomla\CMS\Document\HtmlDocument;
+use Joomla\CMS\Plugin\CMSPlugin;
+
+/**
+ * System plugin for removing generator tag.
+ *
+ * @since  1.0.0
+ */
+class PlgSystemRemoveGenerator extends CMSPlugin
+{
+	/**
+	 * Application instance.
+	 *
+	 * @var    \Joomla\CMS\Application\CMSApplicationInterface
+	 * @since  1.0.0
+	 */
+	protected $app;
+
+	/**
+	 * Flag whether plugin should continue running.
+	 *
+	 * @var    boolean
+	 * @since  1.0.0
+	 */
+	protected $appCheck;
+
+	public function __construct(&$subject, $config)
+	{
+		parent::__construct($subject, $config);
+
+		$this->appCheck = $this->app->isClient('site') || $this->app->isClient('administrator');
+	}
+
+	/**
+	 * Removes generator tag from HTML pages.
+	 *
+	 * @since   1.0.0
+	 */
+	public function onBeforeRender(): void
+	{
+		if (!$this->appCheck)
+		{
+			return;
+		}
+
+		$doc = $this->app->getDocument();
+
+		if (!($doc instanceof HtmlDocument))
+		{
+			return;
+		}
+
+		$doc->setGenerator('');
+	}
+
+	/**
+	 * Removes generator tag from feed pages.
+	 *
+	 * @since   1.0.0
+	 */
+	public function onAfterRender(): void
+	{
+		if (!$this->appCheck)
+		{
+			return;
+		}
+
+		$doc = $this->app->getDocument();
+
+		if (!($doc instanceof FeedDocument))
+		{
+			return;
+		}
+
+		$patterns = [
+			'/<generator.*?<\/generator>\n/',
+			'/<!-- generator=".*?" -->\n/',
+		];
+		$body = preg_replace($patterns, '', $this->app->getBody());
+
+		$this->app->setBody($body);
+	}
+}
